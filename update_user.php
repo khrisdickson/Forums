ae171a617f7dd0424a1aa07a75bafcabb8caf612
Jose <jmbellodelgado@gmail.com>
2013-12-16 13:22:50 -0800

    Update update_user.php
    runs with no errors but wont update database

--- update_user.php
+++ update_user.php
@@ -2,19 +2,23 @@
 <body>
 
 <?php
+include 'connectvars.php';
+include 'header.php';
+
 $conn = mysqli_connect ('webdesign4.georgianc.on.ca','db100083097','871124','db100083097') or die('Error connecting to MySQL server');
 
 	$id = $_POST['userid'];
 	$username = $_POST['username'];
 	$email = $_POST['email'];
 	
-	
-	if (isset($id)) {
-		$sql = "UPDATE Users SET username = '$username',  email = '$email' WHERE id = $userid";
-		mysqli_query($conn, $sql) or die('Error updating database.');
-		mysqli_close($conn);
+	if (isset($_SESSION['userid'])) {
+		
+		$sql = "UPDATE Users SET username = '$username',  email = '$email' WHERE userid = '" . mysql_real_escape_string($_POST['username']) . "'";
+	$result = mysqli_query($conn, $sql) or die('Error updating database.');
+
+		
   
-		header('Location: user_admin_table.php');
+		header('Location: index.php');
 		//desired location ^^^^^^^ header('Location: index.php');
 	}
 	else {

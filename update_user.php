c794744e9998dda51dbd903cef3b0e1a4fda63b8
Jose <jmbellodelgado@gmail.com>
2013-12-16 14:37:22 -0800

    Update update_user.php

--- update_user.php
+++ update_user.php
@@ -1,3 +1,6 @@
+<!-- this execute the information that the user want to changefrom user.php 
+and updates the database with the new information that was added-->
+
 <html>
 <body>
 
@@ -5,15 +8,19 @@
 include 'connectvars.php';
 include 'header.php';
 
+//sets variable to connect to database
 $conn = mysqli_connect ('webdesign4.georgianc.on.ca','db100083097','871124','db100083097') or die('Error connecting to MySQL server');
-
+	
+	//pulls attribute of the <input> tag from the last form
 	$id = $_POST['userid'];
 	$username = $_POST['username'];
 	$email = $_POST['email'];
 	
+	// opens a sesssion if the user has logged in $sql will run if they have not logged it tell them they are invalid and to log in
 	if (isset($_SESSION['userid'])) {
 		
-		$sql = "UPDATE Users SET username = '$username',  email = '$email' WHERE userid = '" . mysql_real_escape_string($_POST['username']) . "'";
+		//updates database with new information
+		$sql = "UPDATE Users SET username = '$username',  email = '$email' WHERE userid = '" . mysql_real_escape_string($_POST['userid']) . "'";
 	$result = mysqli_query($conn, $sql) or die('Error updating database.');
 
 		
@@ -22,6 +29,7 @@ $conn = mysqli_connect ('webdesign4.georgianc.on.ca','db100083097','871124','db1
 		//desired location ^^^^^^^ header('Location: index.php');
 	}
 	else {
+		//if user had not logged in they will see this error message
 		echo 'Invalid user ID.  Click <a href="javascript:history.go(-1)">HERE</a> to go back.';
 	}
 ?>

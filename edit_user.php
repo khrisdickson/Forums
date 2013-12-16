88a44cea929cf85aad09fa86509480c0138a32a1
Jose <jmbellodelgado@gmail.com>
2013-12-16 11:18:13 -0800

    Update edit_user.php

--- edit_user.php
+++ edit_user.php
@@ -2,30 +2,28 @@
 <body>
 
 <?php
+include 'connectvars.php';  
+include 'header.php';
 
-session_start();
-if (empty($_SESSION['userid'])) {
-	header('location:login.html');
-}
-else {
-$conn = mysqli_connect ('webdesign4.georgianc.on.ca','db100083097 ','871124','db100083097') or die('Error connecting to MySQL server');
+(isset($_SESSION['userid']));
 
+$conn = mysqli_connect('localhost', 'db100083097','871124','db100083097') or die('Error connecting to MySQL server.');
 
-$sql = "SELECT * FROM Users WHERE userid = $id;";
-$result = mysqli_query($conn, $sql) or die('Error querying database.');
+$sql = "SELECT * FROM Users";
+$result = mysqli_query($conn, $sql);
 
 echo '<table border="1"><tr><th>User Name</th><th>Password</th><th>Email</th><td>Edit</th></tr>';
   
 while ($row = mysqli_fetch_array($result)) {
     echo '<tr><td>' . $row['username'] . '</td><td>' . $row['password'] . '</td><td>' .$row['email'] . '</td>
-    <td><a href="user.php?id=' . $row['id'] . '">Edit</a></td></tr>';
+    <td><a href="user.php?id=' . $row['userid'] . '">Edit</a></td></tr>';
     
 }
 
 echo '</table>';
 
-mysqli_close($conn);
-}
+
+
 
 
 
